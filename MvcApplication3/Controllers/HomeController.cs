ed22f1474d772240b8089afb1d76b03622f7db4c
urhs127 <shruthi127@gmail.com>
2016-12-29 15:57:40 -0500

    Adding Source files

---
+++ MvcApplication3/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Stream your favorite music anytime anywhere!";
+             var list = new List<SelectListItem>();
+            list.Add(new SelectListItem(){Text = "English", Value="English"});
+            list.Add(new SelectListItem(){Text = "Spanish", Value="Spanish"});
+            list.Add(new SelectListItem(){Text = "Hindi", Value="Hindi"});
+            list.Add(new SelectListItem(){Text = "Tulu", Value="Tulu"});
+            ViewBag.Clients = list;
+            ViewBag.Server = ConfigurationManager.AppSettings["ENVIRONMENT_NAME"].ToString();
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+ 
+    }
+}
---
+++ MvcApplication3/Models/musicdata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication3.Models
+{
+    public class musicdata
+    {
+        public int recordid { get; set; }
+     
+    }
+}
